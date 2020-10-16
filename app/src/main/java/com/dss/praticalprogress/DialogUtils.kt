73fe2e1663aa5931afe48d1787e9cea4205dfd8a
Gustavo Asevedo Vieira <gustavo.asevedo@gmail.com>
2020-10-16 01:40:54 -0300

    first commit

---
+++ app/src/main/java/com/dss/praticalprogress/DialogUtils.kt
@@ -0,0 +1,49 @@
+package com.dss.praticalprogress
+
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.ProgressBar
+import android.widget.ScrollView
+import androidx.core.view.get
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by Gustavo Asevedo on 16/10/2020.
+ */
+
+class DialogUtils {
+    companion object {
+        fun checkRemoveProgress(view: View, activity: Activity) {
+
+            if (view is ViewGroup && view !is RecyclerView && view !is ScrollView) {
+                removeProgress(view)
+                view.isClickable = true
+            } else {
+                removeProgress(view.parent as ViewGroup)
+                (view.parent as ViewGroup).isClickable = false
+            }
+
+            activity.window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+        }
+
+        private fun removeProgress(rootview: ViewGroup) {
+            for (count in 0 until rootview.childCount) {
+                var progressRemoved = false
+                if (rootview[count] is ViewGroup) {
+                    val childView = rootview[count] as ViewGroup
+                    for (countChild in 0 until childView.childCount) {
+                        val view = childView[countChild]
+                        if (view is ProgressBar) {
+                            rootview.removeViewAt(count)
+                            progressRemoved = true
+                            break
+                        }
+                    }
+                }
+                if (progressRemoved) break
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/dss/praticalprogress/ViewProgress.kt
@@ -0,0 +1,63 @@
+package com.dss.praticalprogress
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ScrollView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by Gustavo Asevedo on 16/10/2020.
+ */
+class ViewProgress{
+
+    companion object {
+        fun finishLoad(view: View, activity: Activity){
+
+            DialogUtils.checkRemoveProgress(view,activity)
+        }
+    }
+
+    private lateinit var targetView : View
+    private var progressLayout : Int = 0
+    private lateinit var activity: Activity
+
+    fun with(activity: Activity): ViewProgress {
+        this.activity = activity
+        return this
+    }
+
+    fun on(view : View): ViewProgress {
+        targetView = view
+        return this
+    }
+
+    fun setCustomLayout(layoutId : Int): ViewProgress {
+        progressLayout = layoutId
+        return this
+    }
+
+    fun load(){
+        progressLayout = if (progressLayout == 0) R.layout.dialog_progress_local_white else progressLayout
+        runProgressOnView()
+    }
+
+
+    fun runProgressOnView() {
+        val inflater = LayoutInflater.from(activity)
+
+        if (targetView is ViewGroup && targetView !is RecyclerView && targetView !is ScrollView) {
+            inflater.inflate(R.layout.dialog_progress_local_white, targetView as ViewGroup, true)
+            targetView.isClickable = false
+
+        } else {
+            inflater.inflate(
+                R.layout.dialog_progress_local_white,
+                targetView.parent as ViewGroup, true
+            )
+            (targetView.parent as ViewGroup).isClickable = false
+        }
+    }
+
+}
